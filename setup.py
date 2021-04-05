b8e8cf99d10eff596bf22374714d5c64744e54b6
jarbasal <jarbasai@mailfence.com>
2021-04-05 17:58:15 +0100

    0.2a2

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 
-extras = ["requests", "padatious>=0.4.6", "fann2>=1.0.7"]
 setup(
     name='intentBox',
     version='0.2a2',
@@ -10,5 +9,8 @@ setup(
     author='jarbasai',
     install_requires=["adapt-parser>=0.3.3", "padaos>=0.1.9"],
     author_email='jarbasai@mailfence.com',
+    extras_require={
+        "plugins": ["requests", "padatious>=0.4.6", "fann2>=1.0.7"]
+    },
     description='chatterbox intent parser, extract multiple intents from a single utterance '
 )
